a9c8c5f0e0f18319e6f9025d53adddc3ebbaacde
Alex <yueao@uniwyse.com>
2025-11-21 17:20:58 +0800

    build: 添加publicDir配置到vite.config.js

--- mxgraph-vue3/vite.config.js
+++ mxgraph-vue3/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   root: 'playground',
+  publicDir: '../public',
   server: {
     port: 5174,
     open: true
